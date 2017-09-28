84e257863d532e3a724ac7e5c144a2d93534c4a2
openx2 <xiaoyanghaitao@outlook.com>
2017-09-28 22:15:45 +0800

    完成Tutorial09示例

--- OpenGLTutorial/OpenGLTutorial.cpp
+++ OpenGLTutorial/OpenGLTutorial.cpp
@@ -22,9 +22,9 @@ static void RenderScenceCB()
 	static float scale = 0.0f;
 	scale += 0.001f;
 	Matrix4f world;
-	world.m[0][0] = cosf(scale); world.m[0][1] = -sinf(scale); world.m[0][2] = 0.0f; world.m[0][3] = 0.0f;
-	world.m[1][0] = sinf(scale); world.m[1][1] = cosf(scale); world.m[1][2] = 0.0f; world.m[1][3] = 0.0f;
-	world.m[2][0] = 0.0f; world.m[2][1] = 0.0f; world.m[2][2] = 1.0f; world.m[2][3] = 0.0f;
+	world.m[0][0] = sinf(scale); world.m[0][1] = 0.0f; world.m[0][2] = 0.0f; world.m[0][3] = 0.0f;
+	world.m[1][0] = 0.0f; world.m[1][1] = sinf(scale); world.m[1][2] = 0.0f; world.m[1][3] = 0.0f;
+	world.m[2][0] = 0.0f; world.m[2][1] = 0.0f; world.m[2][2] = sinf(scale); world.m[2][3] = 0.0f;
 	world.m[3][0] = 0.0f; world.m[3][1] = 0.0f; world.m[3][2] = 0.0f; world.m[3][3] = 1.0f;
 	// 将值通过得到的一致变量位置传递给shader
 	glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &world.m[0][0]);
